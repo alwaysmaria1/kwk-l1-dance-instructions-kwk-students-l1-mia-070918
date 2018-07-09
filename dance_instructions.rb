73bd3d392c43d95bc36d2702f577b720ed73fd62
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-09 20:22:48 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -26,6 +26,7 @@ def bob_the_reins
 end 
 def bust_a_move
   put "bust a move"
+end
 def lasso 
   puts "1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left 
   elbow straight left."
