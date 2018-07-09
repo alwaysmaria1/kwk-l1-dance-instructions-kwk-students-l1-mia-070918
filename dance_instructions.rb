2ac588497aaec4511195d8be70ed41c0a29fadf6
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-09 20:05:55 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -28,3 +28,4 @@ def lasso
   puts " 2. Lift right arm up and point right elbow diagonally to the right"
   puts "3. Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song,
   as if you were a cowboy holding a rope lasso."
+  
